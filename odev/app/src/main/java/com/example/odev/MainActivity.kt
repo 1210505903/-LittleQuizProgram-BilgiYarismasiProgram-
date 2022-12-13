f2634b6b6ada506d78f34a502c2e679496bb3602
Graudss <yusuf.guner.766@gmail.com>
2022-12-14 01:37:06 +0300

    updatee
    update

---
+++ odev/app/src/main/java/com/example/odev/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.odev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    TextView tot
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ odev/app/src/main/java/com/example/odev/sorularclas.java
@@ -0,0 +1,40 @@
+package com.example.odev;
+
+public class sorularclas {
+    public static String sorular[]={
+            "Ankara Ne Zaman Bbaşkent Oldu ?",
+            "Çanakkale Zaferinin dahil olduğu savaş hangisidir ?",
+            "Hangi Gezegende Yaşıyoruz ?",
+            "Dünyanın en büyük Okyanusu hangisidir ?",
+            "Bir Gün kaç saniyedir ?",
+            "“Sinekli Bakkal” Romanının Yazarı Aşağıdakilerden Hangisidir?",
+            "Aşağıda Verilen İlk Çağ Uygarlıklarından Hangisi Yazıyı İcat Etmiştir?",
+            "Tsunami Felaketinde En Fazla Zarar Gören Güney Asya Ülkesi Aşağıdakilerden Hangisidir?",
+            "Mustafa Kemal Atatürk’ün Nüfusa Kayıtlı Olduğu İl Hangisidir?",
+            "Aşağıdakilerden Hangisi Dünya Sağlık Örgütünün Kısaltılmış İsmidir?",
+            "Hangi İlimizde Demiryolu Yoktur?",
+            "Hangi Ülkenin İki Tane Başkenti Vardır?",
+
+    };
+    public static String sıklar[][]={
+            {"1924","1921","1923","1929"},
+            {"1.Dünya Savaşı","2.Dünya Savaşı","Trablusgarp Savaşı","İnönü Savaşı"},
+            {"Venüs","Merkür","Dünya","Mars"},
+            {"Atlantik Okyanus","Kuzey Bus Okyanusu","Pasifik Okyanusu","Hint Okyanusu"},
+            {"86000","88600","86400","84600"},
+            {"Reşat Nuri Güntekin","Halide Edip Adıvar","Ziya Gökalp"," Ömer Seyfettin"},
+            {"Hititler","Elamlar","Sümerler","Urartular"},
+            {"Endonezya","Srilanka","Tayland ","Hindistan"},
+            {"Bursa "," Ankara","İstanbul","Gaziantep"},
+            {"Uhw","Unıcef","Who","Nato"},
+            {"Batman","Kütahya","Aydın","Muğla"},
+            {"Güney Afrika","Senegal","El Salvador","Venezuela"}
+
+
+    };
+    public static String cevap[]={
+            "1923","1.Dünya Savaşı","Dünya","Pasifik Okyanusu","86400","Halide Edip Adıvar","Sümerler"," Endonezya","Gaziantep","Unıcef","Muğla","Güney Afrika",
+
+
+    };
+}
